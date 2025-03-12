112356bbe4e43eea8bcd2a64c6d1fc71ae62f7ec
Matheus Eduardo Brisolari <brisolarima@gmail.com>
2025-03-12 13:17:12 -0300

    Primeiro commit pelo Eclipse IDE

---
+++ Meu primeiro projeto- Dio/Meu-primeiro-projeto-Dio/src/main/java/me/dio/Meu/primeiro/projeto_/Dio/MeuPrimeiroProjetoDioApplication.java
@@ -0,0 +1,13 @@
+package me.dio.Meu.primeiro.projeto_.Dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoDioApplication.class, args);
+	}
+
+}
---
+++ Meu primeiro projeto- Dio/Meu-primeiro-projeto-Dio/src/test/java/me/dio/Meu/primeiro/projeto_/Dio/MeuPrimeiroProjetoDioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Meu.primeiro.projeto_.Dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
